9dc9615248575934be8f68f71c5867a7c6ab3067
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-13 11:41:52 +0100

    add second possible execution (to finish)

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,7 +4,7 @@ L'utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga
 */
 
-//ask to the user two string and save them in two variables
+/* //ask to the user two string and save them in two variables
 const firstWord = prompt("Inserisci una parola qualunque");
 const secondWord = prompt("Inserisci un'altra parola qualunque");
 
@@ -21,5 +21,30 @@ if (firstWord.length > secondWord.length) {
   //print it in equal order
   console.log(`Two words have the same length: ${firstWord} and ${secondWord}`);
 }
+ */
 
+
+
+//second possible execution
+
+//ask to the user two string and save them in an array
+const words = [];
+
+words.push(prompt("Inserisci una parola qualunque"));
+console.log(words);
+words.push(prompt("Inserisci un'altra parola qualunque"));
+console.log(words);
+
+
+for (let i = 0; i < words.length; i++) {
+  let shorter = words[i];
+  for (let y = i + 1; y < words.length; y++) {
+
+    if (words[y].length < words[i].length) {
+      shorter = words[y];
+    }
+  }
+  console.log(shorter);
+  words.splice(words.indexOf("shorter"), 1);
+}
 
