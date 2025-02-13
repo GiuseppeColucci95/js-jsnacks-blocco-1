d242efc52dba6e71ccdf3ac69affe2cfbf46ff34
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-13 10:26:14 +0100

    add snack-1

---
+++ snack-1/main.js
@@ -0,0 +1,16 @@
+/* 
+TRACCIA
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+*/
+
+const firstNumber = prompt("Inserisci un numero [es. 1, 2, 3, ...]");
+const secondNumber = prompt("Inserisci un altro numero [es. 1, 2, 3, ...]");
+
+if (firstNumber > secondNumber) {
+  alert(`Il primo numero è maggiore del secondo: ${firstNumber}`);
+} else if (secondNumber > firstNumber) {
+  alert(`Il secondo numero è maggiore del secondo: ${secondNumber}`);
+} else {
+  alert(`I due numeri sono uguali: ${firstNumber} e ${secondNumber}`);
+}
