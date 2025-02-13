7109dc6d31b8454da17ce7e93d4ee08b3eb3812b
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-13 11:20:07 +0100

    add snack-2

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,5 +1,25 @@
 /* 
 TRACCIA
-
+L'utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga
 */
+
+//ask to the user two string and save them in two variables
+const firstWord = prompt("Inserisci una parola qualunque");
+const secondWord = prompt("Inserisci un'altra parola qualunque");
+
+//check the shorter word
+if (firstWord.length > secondWord.length) {
+  //print it in order
+  console.log(secondWord);
+  console.log(firstWord);
+} else if (secondWord.length > firstWord.length) {
+  //print it in other order
+  console.log(firstWord);
+  console.log(secondWord);
+} else {
+  //print it in equal order
+  console.log(`Two words have the same length: ${firstWord} and ${secondWord}`);
+}
+
 
